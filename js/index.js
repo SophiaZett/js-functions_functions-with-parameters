31129d602f218f9035f6767da15f4877eee8e155
Sophia S. <sophia0791@icloud.com>
2023-12-29 14:43:47 +0100

    add 3 solutions

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ console.log("Exercise 1:");
 */
 
 // Uncomment the next line of code and figure out why it does not work:
-// logText;
+logText();
 
 function logText() {
   console.log("This text does not appear. Why?");
@@ -16,7 +16,9 @@ function logText() {
 console.log("-------------");
 console.log("Exercise 2:");
 /*
-2: Look at the functions below. They are almost identical. Can you find a way to generalize them into a new function with an input parameter? Replace the function calls below with your new function.
+2: Look at the functions below. They are almost identical. 
+Can you find a way to generalize them into a new function with an input parameter? 
+Replace the function calls below with your new function.
 */
 
 function greetBob() {
@@ -37,18 +39,41 @@ greetMary();
 
 // --v-- write your code here --v--
 
-function greet() {}
+function greet(name1, name2, name3) {
+  console.log(
+    "Welcome " +
+      name1 +
+      ", " +
+      name2 +
+      ", " +
+      name3 +
+      ", good to see you again!"
+  );
+}
+
+greet("Bob", "Alice", "Mary");
 
 // --^-- write your code here --^--
 
 console.log("-------------");
 console.log("Exercise 3:");
 /*
-3: We log a little separation text for each subtask. This is repetitive code, so lets write a function 'logSeparator' which takes the number of the exercise as a parameter and logs the respective separator. Then use this function to replace the existing separator logs in this javascript file.
+3: We log a little separation text for each subtask. 
+This is repetitive code, so lets write a function 'logSeparator' which takes the number of the
+exercise as a parameter and logs the respective separator. 
+Then use this function to replace the existing separator logs in this javascript file.
 
-Note: Separation text means the text that is logged into the console before each exercise. For example: "-------------" and "Exercise 1:" are the separation texts for the first exercise.
+Note: Separation text means the text that is logged into the console before each exercise. 
+For example: "-------------" and "Exercise 1:" are the separation texts for the first exercise.
 */
 
 // --v-- write your code here --v--
+function logSeparator(exerciseNumber) {
+  console.log("-------------");
+  console.log(`Exercise ${exerciseNumber}:`);
+}
 
+logSeparator(1);
+
+logSeparator(2);
 // --^-- write your code here --^--
